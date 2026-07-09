47da4497072f081cba8278393afe85c5e6f398fc
KohlYumul <kqyumul@student.hau.edu.ph>
2026-07-09 18:54:39 +0800

    Register Project and PersonalInformation models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project, PersonalInformation
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(PersonalInformation)
